4eb86f6af45992084a87ff7d42a51a23d7c4eb3c
Ahmed-El-sayed-Mahmoud <130814088+Ahmed-El-sayed-Mahmoud@users.noreply.github.com>
2023-04-16 08:46:05 +0200

    Update Scheduler.h

--- Process Scheduler/Scheduler.h
+++ Process Scheduler/Scheduler.h
@@ -1,5 +1,15 @@
-#pragma once
+//#include"LinkedQueue.h"
+#include<iostream>
+#include"process.h"
+
+using namespace std;
 class Scheduler
 {
+private:
+	int num_FCFS,num_RR,num_SJF,RTF,MaxW,STL,ForkProb,num_processes;
+	LinkedQueue<mypair<int>> sigKILL;
+	Process**process_ptr;
+public:
+	Scheduler();
+	void LoadInputs();
 };
-
