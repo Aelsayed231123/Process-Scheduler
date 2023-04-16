13f54cdf2f7305461d7ee1bd9ff50c2c4d8de09a
Aelsayed231123 <ahmedelsayed231123@gmail.com>
2023-04-16 06:50:14 +0200

    Processor

--- Process Scheduler/Processor.cpp
+++ Process Scheduler/Processor.cpp
@@ -1,1 +1,22 @@
 #include "Processor.h"
+Processor::Processor(Scheduler* psch)
+{
+	pSch = psch;
+}
+int Processor:: getExpTime()
+{
+	return ExpectedTime;
+}
+Processor* Processor::getRUN()
+{
+	return RUN;
+}
+float Processor::getpLoad()
+{
+	return pLoad;
+}
+float Processor::getpUtil()
+{
+	return pUtil;
+}
+
--- Process Scheduler/Processor.h
+++ Process Scheduler/Processor.h
@@ -1,5 +1,20 @@
 #pragma once
+#include "Scheduler.h"
+enum CPUState{IDLE,BUSY};
 class Processor
 {
+private:
+	Scheduler* pSch;
+	enum CPUState State;
+	float pLoad, pUtil;
+	int ExpectedTime;
+	Processor* RUN;
+public:
+	Processor(Scheduler* psch);
+	virtual void ScheduleAlgo() = 0;
+	int getExpTime();
+	Processor* getRUN();
+	float getpLoad();
+	float getpUtil();
 };
 
