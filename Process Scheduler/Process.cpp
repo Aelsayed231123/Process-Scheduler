5d93c5ed52ced106949d441032b788a0cb7535e2
Ahmed <Ahmed@AF23>
2023-04-16 06:20:49 +0200

    Added Procces class

--- Process Scheduler/Process.cpp
+++ Process Scheduler/Process.cpp
@@ -1,1 +1,77 @@
+#include<iostream>
+#include<cstdlib>
 #include "Process.h"
+Process::Process(int id, int arrT, int cpuT, int n, mypair<int>* P) :pID(id), AT(arrT), CT(cpuT), N(n)
+{
+	RT = -1;
+	TT = -1;
+	TRT = -1;
+	WT = -1;
+	Parptr = nullptr;
+	Childptr = nullptr;
+	for (int i = 0; i < N; i++)
+	{
+		IOqueue.enqueue(P[i]);
+	}
+}
+void Process::set_RT(int readyT)
+{
+	RT = readyT - AT;
+}
+void Process::set_TT(int terT)
+{
+	TT = terT;
+	TRT = TT - AT;
+	WT = TRT - CT;
+}
+int Process::get_AT()
+{
+	return AT;
+}
+int Process::get_RT()
+{
+	return RT;
+}
+int Process::get_CT()
+{
+	return CT;
+}
+int Process::get_TT()
+{
+	return TT;
+}
+int Process::get_TRT()
+{
+	return TRT;
+}
+int Process::get_WT()
+{
+	return WT;
+}
+bool Process::CreateChild(int fp)
+{
+	bool Create = false;
+	srand((unsigned)time(NULL));
+	float r = ((double)rand() / (RAND_MAX));
+	if (r >= fp)
+	{
+		Create = true;
+	}
+	return Create;
+}
+void Process::set_child(Process* c)
+{
+	Childptr = c;
+}
+bool Process::request_IO(int time)
+{
+	int t = time - RT;
+	bool request = false;
+	mypair<int>curIO;
+	if (IOqueue.peek(curIO))
+	{
+		if (curIO.frist == t)
+			request = true;
+	}
+	return request;
+}
--- Process Scheduler/Process.h
+++ Process Scheduler/Process.h
@@ -1,5 +1,36 @@
 #pragma once
+#include"LinkedQueue.h"
+template<class T>
+struct mypair
+{
+	T frist;
+	T second;
+};
 class Process
 {
+	int pID;
+	int AT;
+	int RT;
+	int CT;
+	int TT;
+	int TRT;
+	int WT;
+	int N;
+	LinkedQueue<mypair<int>>IOqueue;
+	Process* Parptr;
+	Process* Childptr;
+public:
+	Process(int id, int arrT, int cpuT, int n, mypair<int>* P);
+	void set_RT(int runT);
+	void set_TT(int terT);
+	int get_AT();
+	int get_RT();
+	int get_CT();
+	int get_TT();
+	int get_TRT();
+	int get_WT();
+	bool CreateChild(int p);
+	void set_child(Process* c);
+	bool request_IO(int time);
 };
 
