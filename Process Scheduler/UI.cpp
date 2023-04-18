2b476f803f50f4c49d5670e854171e31863c2894
Ahmed-El-sayed-Mahmoud <130814088+Ahmed-El-sayed-Mahmoud@users.noreply.github.com>
2023-04-18 23:03:43 +0200

    Update UI.cpp

--- Process Scheduler/UI.cpp
+++ Process Scheduler/UI.cpp
@@ -1,1 +1,25 @@
-#include "UI.h"
+#include"UI.h"
+UI::UI(Scheduler*p)
+{
+	Psch=p;
+}
+void UI::LoadInterface()
+{
+	cout<<"Current TIMESTEP : "<<Psch->get_time_step()<<"\n";
+	cout<<"----------------------------------     READY PROCESSORS       ---------------------------------------------"<<"\n";
+	Psch->Print_Processors_Ready();
+	cout<<"\n";
+	cout<<"-------------------------------------------------     BLK PROCESSES      ------------------------------------"<<"\n";
+	cout<<Psch->get_num_blk()<<"  BLK: ";
+	Psch->Print_Processes_blk();
+	cout<<"\n";
+	cout<<"-------------------------------------------      RUN PROCESSES      ---------------------------------------"<<"\n";
+	cout<<Psch->get_num_run()<<"  RUN: ";
+	Psch->Print_Processes_inRun();
+	cout<<"\n";
+	cout<<"--------------------------------------      TRM PROCESSES      -------------------------------------------------------------------"<<"\n";
+	cout<<Psch->get_num_terminate()<<" TRM: ";
+	Psch->Print_Processes_trem();
+	cout<<endl;
+	cout<<"---------------------------------------------------------------------------------------------------------------------------"<<"\n";
+}
