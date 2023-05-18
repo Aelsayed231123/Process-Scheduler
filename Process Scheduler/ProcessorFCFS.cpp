8ae4a0f0e020e14e11316d99296a46c40e5ed88e
Aelsayed231123 <ahmedelsayed231123@gmail.com>
2023-05-19 01:37:45 +0300

    .

--- Process Scheduler/ProcessorFCFS.cpp
+++ Process Scheduler/ProcessorFCFS.cpp
@@ -32,7 +32,7 @@ void ProcessorFCFS::ScheduleAlgo()
 		{
 			pSch->forK_a_child(RUN);
 		}
-		else if (RUN != nullptr && ((pSch->get_time_step() - RUN->get_AT() - RUN->getRunTime()) > pSch->get_MaxW()))
+		else if (RUN != nullptr && ((pSch->get_time_step() - RUN->get_AT() - RUN->getRunTime()) > pSch->get_MaxW()) && !(RUN->IsChild()))
 		{
 			CheckMigration();
 		}
